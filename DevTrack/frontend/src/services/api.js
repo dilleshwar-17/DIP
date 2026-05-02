a505ddf858b49eb554be582d4a0009d4f0d82f71
dilleshwar-17 <dilleshwar24@gmail.com>
2026-05-02 13:45:19 +0530

    Update frontend api url and fix minor issues

--- DevTrack/frontend/src/services/api.js
+++ DevTrack/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://dip-9xfe.onrender.com/api';
+const API_URL = 'https://dip-5hlo.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
